c019cabd84ea1834884faccece4788f8e5b321bb
danielwijaw <daniel.wija.w@gmail.com>
2020-02-08 09:59:33 +0700

    hidden_bootstrap

--- application/views/frontend/main/index.php
+++ application/views/frontend/main/index.php
@@ -23,7 +23,6 @@
   </style>
   <!-- Bootstrap core JavaScript-->
   <script src="<?php echo base_url() ?>public/vendor/jquery/jquery.min.js"></script>
-  <script src="<?php echo base_url() ?>public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </head>
 
